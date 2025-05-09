ebb5496735f24d319658b4de4cc221d953bef151
Adan222 <kamil.fugiel@s2innovation.com>
2025-05-09 23:07:27 +0200

    Implement check for King moves to ensure safety
    This commit introduces logic to verify if a King move is safe,
    preventing it from moving to squares attacked by enemy pieces.

--- src/main/java/chess/pieces/King.java
+++ src/main/java/chess/pieces/King.java
@@ -10,6 +10,19 @@ import chess.MoveHistory;
 import chess.board.ChessBoard;
 
 public class King extends Piece {
+    private int[][] moves = {
+            { -1, -1 }, // left up corner
+            { 0, -1 }, // up
+            { 1, -1 }, // right up corner
+
+            { 1, 0 }, // right
+            { -1, 0 }, // left
+
+            { -1, 1 }, // left down corner
+            { 0, 1 }, // down
+            { 1, 1 } // right down corner
+    };
+
     public King(Color color) {
         super(color, Piece.Type.King);
     }
@@ -36,18 +49,7 @@ public class King extends Piece {
         List<Move> moveList = new ArrayList<>();
         chess.board.Field[][] board = chessBoard.getBoard();
 
-        int[][] moves = {
-                { -1, -1 }, // left up corner
-                { 0, -1 }, // up
-                { 1, -1 }, // right up corner
-
-                { 1, 0 }, // right
-                { -1, 0 }, // left
-
-                { -1, 1 }, // left down corner
-                { 0, 1 }, // down
-                { 1, 1 } // right down corner
-        };
+        boolean isWhite = this.getColor() == Color.White;
 
         // Look over all moves
         for (int[] dir : moves) {
@@ -68,12 +70,9 @@ public class King extends Piece {
 
         // Castling
         if (!kingMoved(moveHistory)) {
-            Piece king = board[x][y].getPiece();
-            boolean isWhite = king.getColor() == Color.White;
-
             int my = isWhite ? 7 : 0;
 
-            // TODO: Check if Rook moved
+            // TODO: Check if Rook moved or doesn't exist
 
             // Long castling
             int lcx = x - 2;
@@ -90,7 +89,58 @@ public class King extends Piece {
             }
         }
 
-        // TODO: Implement checking move for check
+        // Get list of move for other Pieces and check if these moves are in the
+        // current King move list
+        List<Move> enemyPieceMoves = new ArrayList<>();
+        Color enemyColor = isWhite ? Color.Black : Color.White;
+
+        for (int col = 0; col < 8; col++) {
+            for (int row = 0; row < 8; row++) {
+                Piece piece = board[row][col].getPiece();
+                if (piece != null && piece.getColor() == enemyColor) {
+                    // If we will stumble upon King, this will cause infinity loop
+                    if (piece instanceof King)
+                        enemyPieceMoves.addAll(getEnemyKingMoves(row, col));
+                    else
+                        enemyPieceMoves.addAll(piece.getMoves(row, col, chessBoard, moveHistory));
+                }
+            }
+        }
+
+        // Remove Kings move that has mutual target square - these moves are illegal
+        List<Move> filteredMoves = new ArrayList<>();
+        for (Move move : moveList) {
+            boolean isMutual = false;
+
+            for (Move enemyMove : enemyPieceMoves) {
+                if (move.getTargetX() == enemyMove.getTargetX() &&
+                        move.getTargetY() == enemyMove.getTargetY()) {
+                    isMutual = true;
+                    break;
+                }
+            }
+
+            if (!isMutual) {
+                filteredMoves.add(move);
+            }
+        }
+
+        moveList = filteredMoves;
+        return moveList;
+    }
+
+    /**
+     * It only gets moves around enemy King, nothing else
+     */
+    private List<Move> getEnemyKingMoves(int x, int y) {
+        List<Move> moveList = new ArrayList<>();
+        for (int[] dir : moves) {
+            int dx = dir[0], dy = dir[1];
+            int nx = x + dx, ny = y + dy;
+
+            if ((nx >= 0) && (nx <= 7) && (ny >= 0) && (ny <= 7))
+                moveList.add(new Move(x, y, nx, ny, Move.Type.Movement));
+        }
 
         return moveList;
     }
